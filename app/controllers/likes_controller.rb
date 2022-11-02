d9d4395ae0a453963d1cdce752e7726d462950d1
Mark Coleman <m@rkcoleman.co.uk>
2022-11-02 21:25:00 +0000

    dev: Generate Like model and controller

---
+++ app/controllers/likes_controller.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+class LikesController < ApplicationController
+  def create
+    @like = Like.find_or_create_by(like_params)
+
+    respond_to do |format|
+      if @like.save
+        format.html { render partial: 'articles/like_status',
+                      locals: {article_id: @like.article_id,
+                               liked: true} }
+      else
+        Rails.logger.warn "Could not like article #{like_params[:article_id]}"
+        format.html { head :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @like = Like.where(like_params).first
+    @like&.destroy
+
+    respond_to do |format|
+      format.html { render partial: 'articles/like_status',
+                    locals: {article_id: like_params[:article_id],
+                             liked: false} }
+    end
+  end
+
+  def like_params
+    params.permit(:article_id)
+  end
+end
